e2b60ddca76b3f6bb12f290d4dcc4ee043386e29
Teddy Sears <tjsearsz@users.noreply.github.com>
2020-02-14 09:30:37 -0500

    Homework 2
    - Adding the Homework number two of the android course

---
+++ 2. Layouts/homework2/app/src/main/java/com/teddysears/homework2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.teddysears.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setViewAndChildrenEnabled(findViewById(R.id.cardid1),false);
+        setViewAndChildrenEnabled(findViewById(R.id.cardid2),false);
+    }
+
+    private static void setViewAndChildrenEnabled(View view, boolean enabled) {
+        view.setEnabled(enabled);
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                setViewAndChildrenEnabled(child, enabled);
+            }
+        }
+    }
+}
