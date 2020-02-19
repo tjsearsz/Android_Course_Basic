1d63d45957b416b6a42f03442306bbdad860b7c1
Teddy Sears <tjsearsz@users.noreply.github.com>
2020-02-19 13:55:33 -0500

    Homework 3
    Copying and pasting required files

--- 3.MaterialDesign/app/src/main/java/com/teddysears/homework3/MainActivity.java
+++ 3.MaterialDesign/app/src/main/java/com/teddysears/homework3/MainActivity.java
@@ -3,6 +3,8 @@ package com.teddysears.homework3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViewAndChildrenEnabled(findViewById(R.id.cardid1),false);
+        setViewAndChildrenEnabled(findViewById(R.id.cardid2),false);
+    }
+
+    private static void setViewAndChildrenEnabled(View view, boolean enabled) {
+        view.setEnabled(enabled);
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                setViewAndChildrenEnabled(child, enabled);
+            }
+        }
     }
 }
