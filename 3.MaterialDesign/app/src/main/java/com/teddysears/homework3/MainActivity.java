bfd57c1b1932d4881af1cc43294ad52a1fb47433
Teddy Sears <tjsearsz@users.noreply.github.com>
2020-02-21 13:10:22 -0500

    Floatting button and Styles
    - Adding a floating button
    
    - applying styles to cardviews and date texts

--- 3.MaterialDesign/app/src/main/java/com/teddysears/homework3/MainActivity.java
+++ 3.MaterialDesign/app/src/main/java/com/teddysears/homework3/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,10 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setViewAndChildrenEnabled(findViewById(R.id.cardid1),false);
-        setViewAndChildrenEnabled(findViewById(R.id.cardid2),false);
+        //setViewAndChildrenEnabled(findViewById(R.id.cardid1),false);
+        //setViewAndChildrenEnabled(findViewById(R.id.cardid2),false);
+
+        FloatingActionButton floatingButton = findViewById(R.id.floating1);
+        floatingButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Snackbar Working!", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 
+    /**
+     * Method to enable or disable an element and all its children
+     * @param view The view we will disable and seek
+     * @param enabled Flag to indicate whether the elements will be enabled/disable
+     */
     private static void setViewAndChildrenEnabled(View view, boolean enabled) {
         view.setEnabled(enabled);
         if (view instanceof ViewGroup) {
